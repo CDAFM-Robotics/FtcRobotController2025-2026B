75f77b53aa16a8c3d2e48e37416747665ddd6b33
Jonathan-Witt <jonathanzwitt@gmail.com>
2026-02-22 15:32:29 -0600

    Revert "Revert "remove compiling error""
    This reverts commit 384d99c6e277c1046c392113f48d37fe3eed4336.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/archived/ServoTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/archived/ServoTestOpMode.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.testing;
+package org.firstinspires.ftc.teamcode.testing.archived;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
